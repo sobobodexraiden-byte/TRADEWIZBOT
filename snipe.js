f37e5a9911b2b4cb4cec8dd3edc830abc4ed904a
sobobodexraiden-byte <sobobodexraiden@gmail.com>
2026-05-07 02:11:44 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome to Trade Wiz Trading Bot* 🤖
+🚀 *Welcome to RICK TRADE BOT* 🤖
 *I'm your Web3 execution engine*.
 AI-driven. Battle-tested. Locked down.
 ━━━━━━━━━━━━━━━━━━
